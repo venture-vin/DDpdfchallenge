fa2870e97cf9519a30a12d9d2cc90b6f998b9202
Olya Royall <olya.vgrig+git@gmail.com>
2017-03-08 10:53:46 -0800

    update and clean up functions

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-var button = $("#button");
+var button = $('#button');
 const zoomVal = 16;
 const layer = 'ortho';
 
 // HELPER FUNCTIONS TO HELP GET ALL OF THE DATA TO FEED TO EVENT LISTENER
 
-function getPlanId(){
-  new DroneDeploy({version: 1})
-  .then(function(dronedeployApi){
-    // console.log(dronedeployApi.Plans.getCurrentlyViewed());
-    return dronedeployApi.Plans.getCurrentlyViewed();
-  });
-}
-
 function getTiles(api, plan){
-  return api.Tiles.get({
-    planId: plan.id,
-    layerName: layer,
-    zoom: zoomVal
-  });
+    return api.Tiles.get({
+        planId: plan.id,
+        layerName: layer,
+        zoom: zoomVal
+    });
 }
 
-function sendTileInfo(geo, tileData, zoom, annotations){
+function getAnnotations(api, plan){
+    return api.Annotations.get(plan.id, {comments: true})
+}
+
+function sendRecTileInfo(geo, tileData, zoom, annotations){
   var body = {
     tiles: tileData.tiles,
     planGeometry: geo,
@@ -28,54 +24,35 @@ function sendTileInfo(geo, tileData, zoom, annotations){
     annotations: annotations,
   };
 
-  $.ajax({
-    type: "POST",
+$.ajax({
+    type: 'POST',
     url: 'https://powerful-escarpment-84106.herokuapp.com/',
     data: JSON.stringify(body),
     success: function(response){
-        console.log(response)
+        var file = new Blob([response], {type: 'application/pdf'});
+        var fileURL = URL.createObjectURL(file);
+        window.open(fileURL, '_blank')
     },
     error: function(){
         alert('Error saving image details')
     }
-  });
+});
 
-// // Got sources from https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-// function readBlob(blob){
-//   return new Promise ((resolve) => {
-//     var reader = new FileReader();
-//     reader.onload = () => resolve(reader);
-//     reader.readAsBinaryString(blob);
-//   })
-// }
-
-// // Use jsPDF based on documentation here: http://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html
-// function downloadPDF(reader) {
-//   var doc = new jsPDF();
-//   doc.addImage(img, 'JPEG', 20, 20);
-//   doc.save('MAP.pdf')
-// };
 
 // FUNCTION TO ENCAPSULATE ALL HELPER FUNCTIONS ABOVE
 
-
 function genPDF(){
-  new DroneDeploy({version: 1}).then(function(dronedeployApi){
-    return dronedeployApi.Plans.getCurrentlyViewed().then(function(plan){
-      return getTiles(dronedeployApi, plan);
-    });
-  })
-    .then(planId => getAnnotations(planId))
-    .then(annotations => sendTileInfo(plan.geometry, tile, zoom, annotations))
-    .then(response => handleResponse(response))
-    .then(reader => downloadPDF)
+    new DroneDeploy({version: 1}).then(function(dronedeployApi){
+        return dronedeployApi.Plans.getCurrentlyViewed()
+            .then(plan => getTiles(dronedeployApi, plan)
+            .then(plan => getAnnotations(plan))
+            .then(annotations => sendRecTileInfo(plan.geometry, tile, zoom, annotations))
+        })
 }
 
 
 button.on("click", function(event){
-  console.log("clicked!", event);
-  console.log(plan);
-  console.log(tileData)
+    genPDF();
 });
 
 genPDF();
